d5494f34ef03dcbc17e49429c1c1362e9abbc43a
econ <rycong@gmail.com>
2022-09-02 10:57:43 -0700

    beginning changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarLoginComponent } from './components/navbar-login/navbar-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: "landing",
+  component: LandingComponent
+},
+
+{
+  path: "login",
+  component: LoginComponent
+},
+
+{
+  path: "signup",
+  component: SignupComponent
+},
+
+{
+  path: '',
+  component: LandingComponent
+}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
