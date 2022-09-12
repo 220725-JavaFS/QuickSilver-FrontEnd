57eefb9eb4edd2bd51ce47b2a7063b1528d6a5f4
jorropowathil <jorispowathil@gmail.com>
2022-09-12 15:08:52 -0500

    about me component stuff

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,22 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit { 
 
-  constructor() { }
+  
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
   }
 
 
   //function for grabbing most recently completed workout and return an object
-    //will use this object to populate most recent workout
+  //will use this object to populate most recent workout
+
+  getLastWorkout(id:number){
+    // return this.httpClient.get(this.url,{
+    //   headers:{
+    //     accept: "application/json"
+    //   }
+    // }) as Observable<workout>;
+
+  }
+
 
   //Function for grabbing user calorie goal, and calories burned for the day 
+  getCalorieProgess(){
+
+  } 
+
+
 
   // function to check how many days in a row that calories goal has been met
+  getStreakCount(){
+    
+  }
+
+
 }
