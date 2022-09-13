323c767ca3d751beaa58b0c0825eb3f0e9f5aa95
unknown <jarib.rivas@gmail.com>
2022-09-13 02:35:29 -0700

    fia

---
+++ src/app/exercise-complete.spec.ts
@@ -0,0 +1,7 @@
+import { ExerciseComplete } from './exercise-complete';
+
+describe('ExerciseComplete', () => {
+  it('should create an instance', () => {
+    expect(new ExerciseComplete()).toBeTruthy();
+  });
+});
