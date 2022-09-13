0dbd0fd89896f683144a5fbdfc69f209bb0ad7f1
jorropowathil <jorispowathil@gmail.com>
2022-09-13 01:25:20 -0500

    got access to user calorie goal

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import {ExerciseComplete} from '../../exercise-complete';
+import { ExerciseComplete } from '../../exercise-complete';
 import { Exercise } from 'src/app/exercise';
-import { Router } from '@angular/router';
+import { AccountSingletonModule } from 'src/app/account-singleton/account-singleton.module';
 import { LoginService } from 'src/app/services/login.service';
+
 
 
 @Component({
@@ -12,80 +13,94 @@ import { LoginService } from 'src/app/services/login.service';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+export class AboutComponent implements OnInit {
 
-export class AboutComponent implements OnInit { 
+  constructor(private httpClient: HttpClient, private loginService: LoginService) { }
+
+  exercises: ExerciseComplete[] = [];
+  account: AccountSingletonModule = new AccountSingletonModule;
   
-  constructor(private httpClient:HttpClient) { }
-  exercises:ExerciseComplete[] = [];
   ///////////////////////////////////////
+  lastWorkoutDate: String = "";
   lastWorkoutName: String = "";
   lastWorkoutDuration: number = 0;
   lastWorkoutCalories: number = 0;
+  date: Date = new Date;
   //////////////////////////////////////////
-  dailyCalorieGoal: number = 0;
+  // dailyCalorieGoal: number = 0;
+  dailyCalorieGoal: String = "";
   caloriesBurnedToday: number = 0;
   //caloriesBurnedProgress
-
-  ngOnInit(): void {
-    this.getLastWorkout().subscribe(
-      (response:ExerciseComplete[])=>{
-        this.exercises=response;
-        console.log(response);
-        console.log("Date of your most recent workout: ", response[response.length-1].date);
-        console.log("Duration of your most recent workout: ", response[response.length-1].durMin);
-        console.log("Name of your most recent workout: ", response[response.length-1].name);
-        console.log("Calories burned during your most recent workout: ", response[response.length-1].totCal);
-        ////////////////////////////////////////////////////////////////
-        
-      }
-    );
-  }
-
-
-  //function for grabbing most recently completed workout and return an object
-  //will use this object to populate most recent workout
-
-  url:string = "http://localhost:8084/data/workout/getWorkouts";
-  //Pass in loged user id?
-
-  // id:number
-  getLastWorkout():Observable<ExerciseComplete[]>{
-    return this.httpClient.get(this.url,{
-        headers:{
-        accept:"application/json"
-      }
-    }) as Observable<ExerciseComplete[]>;
-    
-    
-export class AboutComponent implements OnInit {
-
-  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.checkIfUserIsLoggedIn();
   }
 
-  checkIfUserIsLoggedIn(){
+  checkIfUserIsLoggedIn() {
     this.loginService.checkUserLogin();
-
+    this.getMostRecentWorkoutInfo();
+    this.getAccountInfo();
+    this.getCalorieGoal();
+    console.log(this.getAccountInfo());
+    console.log(AccountSingletonModule.clientId);
+    console.log(AccountSingletonModule.email.toString());
   }
 
 
+  url: string = "http://localhost:8084/data/workout/getWorkouts";
+  // id:number
+  getLastWorkout(): Observable<ExerciseComplete[]> {
+    return this.httpClient.get(this.url, {
+      headers: {
+        accept: "application/json"
+      }
+    }) as Observable<ExerciseComplete[]>;
+  }
+  
+  //Pass in loged user id?
+  getMostRecentWorkoutInfo() {
+    this.getLastWorkout().subscribe(
+      (response: ExerciseComplete[]) => {
+        this.exercises = response;
+        this.lastWorkoutName = response[response.length - 1].name.toString();
+        this.lastWorkoutDuration = response[response.length - 1].durMin;
+        this.lastWorkoutCalories = response[response.length - 1].totCal;
+        this.date = response[response.length - 1].date
+        console.log(response);
+        console.log("Date of your most recent workout: ", response[response.length - 1].date);
+        console.log("Duration of your most recent workout: ", response[response.length - 1].durMin);
+        console.log("Name of your most recent workout: ", response[response.length - 1].name);
+        console.log("Calories burned during your most recent workout: ", response[response.length - 1].totCal);
+      }
+    );
+  }
 
 
-
-
+  url2: String = "/account";
   //Function for grabbing user calorie goal, and calories burned for the day 
-  getCalorieProgess(){
+  getAccountInfo(): Observable<AccountSingletonModule[]> {
+    return this.httpClient.get(this.url, {
+      headers: {
+        accept: "application/json"
+      }
+    }) as Observable<AccountSingletonModule[]>;
+  }
 
-
-  } 
-
-
+ getCalorieGoal(){
+  this.getAccountInfo().subscribe(
+    (res: AccountSingletonModule[]) => {
+      // this.date = response[0];
+      // this.caloriesBurnedToday = 
+      this.dailyCalorieGoal= res[1].toString();
+      
+    }
+  );
+ }
+ 
 
   // function to check how many days in a row that calories goal has been met
-  getStreakCount(){
-    
+  getStreakCount() {
+
   }
 
 
